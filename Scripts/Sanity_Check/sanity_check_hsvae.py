5911d89fdd82b7a162a73444b8169302b6005232
Victor <victor@example.com>
2020-08-11 19:26:41 +0100

    -

--- Scripts/Sanity_Check/sanity_check_hsvae.py
+++ Scripts/Sanity_Check/sanity_check_hsvae.py
@@ -46,7 +46,29 @@ class Sparse_Concrete_tf(tf.distributions.Distribution):
         U = tf.random_uniform(shape, minval=0, maxval=1)
         return tf.math.log(U+eps) - tf.math.log1p(-U+eps)
     
-    
+    def sanity_check_count_ones_zeros(self, sample):
+       samples = sample.numpy().tolist()
+       #print(sample)
+       prob_stat = [defaultdict(lambda:0), defaultdict(lambda:0)]
+       print(len(sample[0]))
+       for sample in samples:
+           for ex_id, example in enumerate(sample):
+               for val_id, val in enumerate(example):
+                 #  print(val)
+                   prob_stat[ex_id][val_id] += val
+
+       for ex_id, prob in enumerate(prob_stat):
+               for val_id, _ in enumerate(prob):
+                 #  print(val)
+                   prob_stat[ex_id][val_id] /= len(samples)
+
+
+      # for row in sample:
+      #     for idx, val in enumerate(row):
+      #         prob_stat[idx] += val
+      # for idx in prob_stat:
+      #      prob_stat[idx] /= len(sample)
+       return prob_stat
     
     def sample_binary_concrete(self, shape, probs, temperature=1.5, hard=True, eps=1e-20):
         L = self.sample_logistic(shape, eps=eps)
@@ -58,7 +80,8 @@ class Sparse_Concrete_tf(tf.distributions.Distribution):
         if hard:
             result = tf.where(sample <= 0.5,  tf.zeros_like(sample),  tf.ones_like(sample))
             sample = tf.stop_gradient(result-sample)+sample
-       # print('sample',sample)
+   #     print('sample', sample)
+        print('probs', self.sanity_check_count_ones_zeros(sample))
         return sample
     
     def straight_through(self):
@@ -101,7 +124,6 @@ class Sparse_tf(tf.distributions.Distribution):
     def __init__(self, gamma, loc, scale):
         self._name = 'Sparse Dist'
         self.gamma = tf.convert_to_tensor(gamma, dtype=np.float32)
-        #print('gamma',self.gamma)
         self.alpha = tf.convert_to_tensor(0.05, dtype=np.float32)
         self.loc = tf.convert_to_tensor(loc, dtype=np.float32)
         self.scale = tf.convert_to_tensor(scale, dtype=np.float32)
@@ -110,16 +132,13 @@ class Sparse_tf(tf.distributions.Distribution):
                 dtype=self.scale.dtype,
                 reparameterization_type=tf.distributions.NOT_REPARAMETERIZED,
                 validate_args=False,
-                allow_nan_stats=True)
+                allow_nan_stats=False)
 
     def _batch_shape_tensor(self):
-      return array_ops.broadcast_dynamic_shape(
-        array_ops.shape(self.loc),
-        array_ops.shape(self.scale))
+      return array_ops.broadcast_dynamic_shape(array_ops.shape(self.loc), array_ops.shape(self.scale))
 
     
-    def sample(self, n):
-        
+    def sample(self, n): 
         shape = tf.concat([[n], self._batch_shape_tensor()], axis=0)
         epsilon_2 = tf.keras.backend.random_normal(shape=shape, mean=0., stddev=1)
         epsilon_1 = tf.keras.backend.random_normal(shape=shape, mean=0., stddev=1)
@@ -200,6 +219,18 @@ def test_hoyer_for_n_inputs(n):
 
 
 
+def test_concrete_distribution(n_sample):
+    dims = 5
+    gamma =np.array( [[0.8,0.,0.1,0.5,0.3],  [0.1,0.2,0.9,0.8,0.1]], dtype=np.float32 )
+    loc = np.full((1,dims), 0, dtype=np.float32)
+    scale = np.full((1,dims), 1., dtype=np.float32)
 
+    sp = Sparse_Concrete_tf(gamma, loc, scale)
+    sp.sample(n_sample, temperature=0.5)
+
+
+            
+            
 if __name__ == '__main__':
     test_hoyer_for_n_inputs(5)
+    test_concrete_distribution(1000)
